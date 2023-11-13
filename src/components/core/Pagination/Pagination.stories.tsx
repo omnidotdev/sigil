cc89f75a6fb2a019253af30e3f17ce7acf0505e0
Brian Cooper <brian@brian-cooper.com>
2023-11-12 19:26:14 -0600

    style(pagination,stories): add flex wrapper decorator

--- src/components/core/Pagination/Pagination.stories.tsx
+++ src/components/core/Pagination/Pagination.stories.tsx
@@ -1,4 +1,5 @@
 import { Pagination } from "components";
+import { Flex } from "generated/panda/jsx";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
@@ -8,6 +9,13 @@ const meta = {
   title: "Components/Core/Pagination",
   component: Pagination,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <Flex>
+        <Story />
+      </Flex>
+    ),
+  ],
 } satisfies Meta<typeof Pagination>;
 
 export const Default: Story = {
