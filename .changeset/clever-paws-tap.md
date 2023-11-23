1c25e593051d197950588b3ffbd07f81047266a0
Brian Cooper <20056195+coopbri@users.noreply.github.com>
2023-11-22 22:05:52 -0600

    File upload component (#28)
    * feature: add file upload component, stories, and recipe
    
    * ci(changesets): add changeset

---
+++ .changeset/clever-paws-tap.md
@@ -0,0 +1,5 @@
+---
+"@omnidev/sigil": minor
+---
+
+Add `FileUpload` component for uploading files in a dropzone
---
+++ src/components/core/FileUpload/FileUpload.stories.tsx
@@ -0,0 +1,44 @@
+import { Button, FileUpload } from "components";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+type Story = StoryObj<typeof meta>;
+
+const meta = {
+  title: "Components/Core/FileUpload",
+  component: FileUpload,
+  tags: ["autodocs"],
+} satisfies Meta<typeof FileUpload>;
+
+/**
+ * By default, a single file can be uploaded.
+ */
+export const Default: Story = {
+  args: {
+    label: "Drop a file here!",
+    dialogTrigger: <Button size="sm">Choose a file...</Button>,
+  },
+};
+
+/**
+ * The label and dialog trigger text are optional. Clicking anywhere in the dropzone will open the file dialog.
+ */
+export const BlankDropzone: Story = {
+  args: {
+    label: undefined,
+    dialogTrigger: undefined,
+  },
+};
+
+/**
+ * A maximum amount of files can be specified with the `maxFiles` prop.
+ */
+export const MaxFiles: Story = {
+  args: {
+    ...Default.args,
+    label: "Drop files here! (max 3)",
+    maxFiles: 3,
+  },
+};
+
+export default meta;
