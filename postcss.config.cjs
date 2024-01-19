1ea68e121669d18186a8fc84576c4a94894f42cb
Brian Cooper <brian@brian-cooper.com>
2024-01-19 02:33:13 -0600

    feature: add PostCSS config for Lightning CSS

---
+++ postcss.config.cjs
@@ -0,0 +1,6 @@
+// Lightning CSS automatically picks up this PostCSS config (https://lightningcss.dev/docs.html#with-parcel)
+module.exports = {
+  plugins: {
+    "@pandacss/dev/postcss": {},
+  },
+};
