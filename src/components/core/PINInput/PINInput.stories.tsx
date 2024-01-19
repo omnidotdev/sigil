df111d7c3e98a54027a2a5296e56cbdf424d8c49
Brian Cooper <brian@brian-cooper.com>
2024-01-19 01:49:30 -0600

    feature(pin-input,stories): add PIN input stories

--- src/components/core/PINInput/PINInput.stories.tsx
+++ src/components/core/PINInput/PINInput.stories.tsx
@@ -1,17 +1,20 @@
 import { PINInput } from "components";
+import { Stack } from "generated/panda/jsx";
+import { pinInput } from "lib/theme/extensions/slotRecipes";
 
 import type { Meta, StoryObj } from "@storybook/react";
+import type { PinInputVariant as PINInputVariant } from "generated/panda/recipes";
 type Story = StoryObj<typeof meta>;
 
 const meta = {
   title: "Components/Core/PINInput",
   component: PINInput,
   tags: ["autodocs"],
-} satisfies Meta<typeof PINInput>;
+} satisfies Meta;
 
 export const Default: Story = {
   args: {
-    onComplete: (evt) => alert(evt.valueAsString),
+    onValueComplete: (evt) => alert(evt.valueAsString),
   },
 };
 
@@ -21,5 +24,35 @@ export const WithLabel: Story = {
     label: "Enter your PIN below 🔒",
   },
 };
+
+export const WithPlaceholder = () => (
+  <Stack>
+    <PINInput {...Default.args} label="Dots" placeholder="•" />
+    <PINInput {...Default.args} label="Zeros" placeholder="0" />
+  </Stack>
+);
+
+/**
+ * A custom length of input slots can be specified with the `length` prop.
+ */
+export const CustomLength: Story = {
+  args: {
+    ...Default.args,
+    length: 6,
+    label: "Enter your 6-digit PIN below 🔒",
+  },
+};
+
+const sizes = Object.keys(pinInput.variants!.size) as PINInputVariant["size"][];
+
+export const Sizes = () => (
+  <Stack>
+    {sizes.map((size) => (
+      <PINInput key={size} size={size} label={size}>
+        {size}
+      </PINInput>
+    ))}
+  </Stack>
+);
 
 export default meta;
