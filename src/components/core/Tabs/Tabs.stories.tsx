5726aa6169ac64d65dc98b1897527e4daf13b032
Brian Cooper <brian@brian-cooper.com>
2024-02-09 04:15:10 -0600

    feature(stories,tabs): expand tabs stories

--- src/components/core/Tabs/Tabs.stories.tsx
+++ src/components/core/Tabs/Tabs.stories.tsx
@@ -1,8 +1,12 @@
-import { Tabs } from "components";
+import { Table, TableCell, TableHeader, TableRow, Tabs } from "components";
+import { tabs } from "lib/theme/extensions/slotRecipes";
 
 import type { Meta, StoryObj } from "@storybook/react";
+import type { TabsVariant } from "generated/panda/recipes";
 
 type Story = StoryObj<typeof meta>;
+
+// TODO compress horizontal & vertical into one story for each variant
 
 const meta = {
   title: "Components/Core/Tabs",
@@ -62,6 +66,21 @@ export const OutlineVertical: Story = {
   },
 };
 
+export const Enclosed: Story = {
+  args: {
+    ...Default.args,
+    variant: "enclosed",
+  },
+};
+
+export const EnclosedVertical: Story = {
+  args: {
+    ...Default.args,
+    orientation: "vertical",
+    variant: "enclosed",
+  },
+};
+
 export const Disabled: Story = {
   args: {
     ...Default.args,
@@ -76,5 +95,74 @@ export const Disabled: Story = {
     ],
   },
 };
+
+const sizes = Object.keys(tabs.variants!.size) as TabsVariant["size"][];
+
+export const Sizes = () => (
+  <Table
+    headerContent={
+      <TableRow css={{ "& *": { fontWeight: "bold" } }}>
+        <TableHeader>Size</TableHeader>
+
+        {sizes.map((size) => (
+          <TableHeader key={size}>{size}</TableHeader>
+        ))}
+      </TableRow>
+    }
+  >
+    <TableRow>
+      <TableCell fontWeight="semibold">Line (Default)</TableCell>
+
+      {sizes.map((size) => (
+        <TableCell key={size} py={2}>
+          <Tabs
+            {...Default.args}
+            size={size}
+            tabs={Default.args.tabs.map((tab) => ({
+              ...tab,
+              label: tab.label,
+            }))}
+          />
+        </TableCell>
+      ))}
+    </TableRow>
+
+    <TableRow>
+      <TableCell fontWeight="semibold">Outline</TableCell>
+
+      {sizes.map((size) => (
+        <TableCell key={size} py={2}>
+          <Tabs
+            {...Default.args}
+            size={size}
+            variant="outline"
+            tabs={Default.args.tabs.map((tab) => ({
+              ...tab,
+              label: tab.label,
+            }))}
+          />
+        </TableCell>
+      ))}
+    </TableRow>
+
+    <TableRow>
+      <TableCell fontWeight="semibold">Enclosed</TableCell>
+
+      {sizes.map((size) => (
+        <TableCell key={size} py={2}>
+          <Tabs
+            {...Default.args}
+            size={size}
+            variant="enclosed"
+            tabs={Default.args.tabs.map((tab) => ({
+              ...tab,
+              label: tab.label,
+            }))}
+          />
+        </TableCell>
+      ))}
+    </TableRow>
+  </Table>
+);
 
 export default meta;
