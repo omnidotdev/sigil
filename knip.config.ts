c3394769b4c67b953728d16ac93abf8941cb2bd2
Brian Cooper <brian@brian-cooper.com>
2024-06-22 02:04:52 -0500

    feature: add knip config

---
+++ knip.config.ts
@@ -0,0 +1,30 @@
+import type { KnipConfig } from "knip";
+
+// TODO integrate `knip` into CI
+
+/**
+ * Knip configuration.
+ * @see https://knip.dev/overview/configuration
+ */
+const knipConfig: KnipConfig = {
+  ignoreDependencies: [
+    // used for local packaging
+    "@omnidev/knit",
+    // used in MDX
+    "@storybook/addon-docs",
+    "@storybook/blocks",
+  ],
+  ignore: [
+    "panda.config.ts",
+    "*.config.*",
+    ".github/**",
+    "examples/**",
+    // `ComponentVariants` marked as unused, but it is required for downstream type mapping
+    "src/lib/util/createStyleContext.tsx",
+    // theme extensions are used in barrels
+    "src/lib/theme/extensions/**",
+    "src/lib/config/env.config.ts",
+  ],
+};
+
+export default knipConfig;
