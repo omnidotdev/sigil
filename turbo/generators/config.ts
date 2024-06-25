58b2b9bd5fd2187879feac3645d44e794581abc9
Brian Cooper <brian@brian-cooper.com>
2024-06-24 23:52:39 -0500

    feature(turbo-gen): add input validation, dynamic recipe rendering

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,32 +1,52 @@
 import type { PlopTypes } from "@turbo/gen";
 
 // TODO automated tests for generator UX
+// TODO change startup turbo gen message
+// TODO bypass for DX (https://plopjs.com/documentation/#bypassing-prompts)
 
 /**
  * Turbo generator configuration, internally based on Plop.
  * @see https://turbo.build/repo/docs/guides/generating-code
  */
-const turboGeneratorConfig = (plop: PlopTypes.NodePlopAPI): void => {
-  plop.setGenerator("component", {
+const turboGeneratorConfig = (config: PlopTypes.NodePlopAPI): void => {
+  config.setGenerator("component", {
     description: "Create files for a new Sigil component",
     prompts: [
-      // TODO validation
       {
-        type: "input",
+        type: "list",
         name: "category",
-        message:
-          // TODO list of categories
-          "What category should the component be in (e.g. core, typography, layout)?",
+        message: "What category should the component be in?",
+        // TODO dynamic default from array
+        default: "core",
+        // TODO dynamic list of categories from filesystem
+        choices: ["core", "typography"],
       },
       {
         type: "input",
         name: "name",
         message: "What is the name of the new component?",
+        validate: (input) =>
+          // only allow alphabetic characters
+          /^[a-zA-Z]+$/.test(input) ||
+          // TODO fix process not exiting on validation error
+          "Invalid component name, only alphabetic characters are allowed",
+      },
+      {
+        type: "list",
+        name: "recipeType",
+        message: "What type of Panda style recipe will this component use?",
+        // TODO dynamic default from array
+        // default: "config (`defineRecipe`)",
+        choices: [
+          { name: "config (`defineRecipe`)", value: "recipe" },
+          { name: "slot (`defineSlotRecipe`)", value: "slotRecipe" },
+        ],
       },
     ],
-    // TODO recipes, update index files
+    // TODO update index files (recipe & component)
     actions: (data) => {
-      const pascalName = plop.getHelper("pascalCase")(data?.["name"]);
+      const camelName = config.getHelper("camelCase")(data?.["name"]),
+        pascalName = config.getHelper("pascalCase")(data?.["name"]);
 
       return [
         {
@@ -34,9 +54,17 @@ const turboGeneratorConfig = (plop: PlopTypes.NodePlopAPI): void => {
           base: "templates/component",
           templateFiles: "templates/component/**",
           destination: `src/components/{{ camelCase category }}/${pascalName}`,
-          data: { pascalName },
         },
-      ];
+        {
+          type: "add",
+          path: `src/lib/theme/extensions/{{ recipeType }}s/{{ camelName }}.{{ recipeType }}.ts`,
+          templateFile: "templates/recipe/{{ recipeType }}.ts.hbs",
+        },
+      ].map((action) => ({
+        ...action,
+        // inject custom data into each action
+        data: { camelName, pascalName },
+      }));
     },
   });
 };
