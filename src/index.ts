c8ef6e5a210fb0f8a3e74010fdaecbcc54bb8861
Brian Cooper <brian@brian-cooper.com>
2024-10-15 17:24:59 -0500

    chore(lint): add disable line rules

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,24 @@ export { styled as sigil } from "generated/panda/jsx";
 // export Panda presets (to be used in downstream Panda configurations)
 export { sigilPreset } from "lib/theme/presets";
 
+// NB: lint disable line due to `useToggle` conflict between `usehooks-ts` and `@ark-ui/react`
+// eslint-disable-next-line import/export
 export * from "lib/hooks";
+
+// NB: below resolves `useToggle` conflict between `usehooks-ts` and `@ark-ui/react`
+// eslint-disable-next-line import/export
+export { useToggle } from "usehooks-ts";
+export { useToggle as useToggleButton } from "@ark-ui/react";
 
 // @ts-expect-error ts(2308): warns about duplicate exports, but intended for composable overrides
 // eslint-disable-next-line import/export
 export * from "components";
+
+// TODO add `CheckboxGroup` component *maybe*, part of Ark UI v3.3.0+
+// TODO add field component? https://ark-ui.com/react/docs/components/field
+// TODO add fieldset component? https://ark-ui.com/react/docs/components/fieldset
+// TODO add highlight component?
+// TODO add steps component (https://ark-ui.com/react/docs/components/steps)
+// TODO add timer component (https://ark-ui.com/react/docs/components/timer)
+// TODO add frame component?
+// TODO add toggle component? https://github.com/chakra-ui/ark/blob/main/packages/react/CHANGELOG.md#added
