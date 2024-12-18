941c369bf46027f16e4952bff896dba041e962f7
Brian Cooper <20056195+coopbri@users.noreply.github.com>
2024-12-18 17:50:40 -0600

    Nullable `Avatar` `name` prop (#139)
    * feature(lib,types): add 'Nullable' type
    
    * docs(lib,types): update 'AssignJSXStyleProps' JSDoc
    
    * fix(avatar,stories): fix type error
    
    * fix(avatar): make 'name' prop nullable

---
+++ src/lib/types/Nullable.type.ts
@@ -0,0 +1,7 @@
+/**
+ * Container type that represents a value that can be null.
+ * @template T Type of value.
+ */
+type Nullable<T> = T | null;
+
+export default Nullable;
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,1 +1,2 @@
 export type { default as AssignJSXStyleProps } from "./AssignJSXStyleProps.type";
+export type { default as Nullable } from "./Nullable.type";
