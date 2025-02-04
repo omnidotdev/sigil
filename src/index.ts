51e0e8af770ddd6c6e5bfac9db2255f9a934910d
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-02-03 21:04:43 -0600

    chore: revert override changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // inject root cascade layers
 import "lib/styles/main.css";
 
+// export backfill of Ark components (overrides allowed below)
+// eslint-disable-next-line import/export
+export * from "@ark-ui/react";
+
 // 🐼 export backfill of Panda components
 export * from "generated/panda/css";
 // eslint-disable-next-line import/export
@@ -17,6 +21,11 @@ export { sigilPreset } from "lib/theme/presets";
 // eslint-disable-next-line import/export
 export * from "lib/hooks";
 
+// NB: below resolves `useToggle` conflict between `usehooks-ts` and `@ark-ui/react`
+// eslint-disable-next-line import/export
+export { useToggle } from "usehooks-ts";
 export { useToggle as useToggleButton } from "@ark-ui/react";
 
+// @ts-expect-error ts(2308): warns about duplicate exports, but intended for composable overrides
+// eslint-disable-next-line import/export
 export * from "components";
