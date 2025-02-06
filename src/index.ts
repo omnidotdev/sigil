dba7146acbba3d8505227620f126398dff109111
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-02-05 22:31:44 -0600

    refactor: update backfills

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 // inject root cascade layers
 import "lib/styles/main.css";
 
-export { createListCollection } from "@ark-ui/react";
+export * from "components";
 
 // 🐼 export backfill of Panda components
+// TODO: determine if this causes named export issue
 export * from "generated/panda/css";
-// eslint-disable-next-line import/export
 export * from "generated/panda/jsx";
+
 export type { JsxStyleProps } from "generated/panda/types";
 
 // NB: `jsxFactory` in Panda config can be specified to customize the styled factory export name, however the default (`styled`) is intended, and this alias is exported as well
@@ -14,14 +15,3 @@ export { styled as sigil } from "generated/panda/jsx";
 
 // export Panda presets (to be used in downstream Panda configurations)
 export { sigilPreset } from "lib/theme/presets";
-
-// NB: lint disable line due to `useToggle` conflict between `usehooks-ts` and `@ark-ui/react`
-// eslint-disable-next-line import/export
-export * from "lib/hooks";
-
-// NB: below resolves `useToggle` conflict between `usehooks-ts` and `@ark-ui/react`
-// eslint-disable-next-line import/export
-export { useToggle } from "usehooks-ts";
-export { useToggle as useToggleButton } from "@ark-ui/react";
-
-export * from "components";
