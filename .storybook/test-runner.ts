996e21380f853537a4cc941aa2b7b219de0e26ae
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-03-06 16:03:49 -0600

    docs(storybook): update test-runner JSDoc
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -2,8 +2,8 @@ import type { TestRunnerConfig } from "@storybook/test-runner";
 
 const config: TestRunnerConfig = {
   /* Hook to execute before a story is initially visited before being rendered in the browser.
-   * The page argument is the Playwright's page object for the story.
-   * The context argument is a Storybook object containing the story's id, title, and name.
+   * The page argument is Playwright's page object for the story.
+   * The context argument is a Storybook object containing the story's ID, title, and name.
    */
   async preVisit(page, _context) {
     await page.content();
