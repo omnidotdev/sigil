baaf1f0904686e002898a3f4998f152d9c29075a
Brian Cooper <brian@brian-cooper.com>
2025-03-20 10:46:59 -0500

    build(deps,scripts): upgrade dependencies, convert to Bun builder

---
+++ src/scripts/build.ts
@@ -0,0 +1,36 @@
+import { $ } from "bun";
+
+/**
+ * Build the library.
+ */
+const build = async () => {
+  await $`rm -rf build/*`;
+
+  // preflight
+  console.log("Generating Panda artifacts...");
+  await $`bun panda codegen`;
+  console.log("Panda artifacts generated.");
+
+  // bundle
+  console.log("Bundling...");
+  await Bun.build({
+    entrypoints: ["src/index.ts"],
+    outdir: "build",
+    external: ["@ark-ui/react", "next", "react", "react-icons"],
+  });
+  console.log("Bundling complete.");
+
+  // postflight
+  console.log("Generating TypeScript declarations...");
+  await $`tsc -d --declarationDir build --declarationMap --emitDeclarationOnly --project tsconfig.build.json`;
+  console.log("TypeScript declarations generated.");
+
+  console.log("Publishing local package...");
+  await $`bun knit push`;
+  console.log("Publishing local package complete.");
+};
+
+build().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
