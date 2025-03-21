76d7c51d323b6ab5998dda6f0ea375fcf3dc47cb
Brian Cooper <brian@brian-cooper.com>
2025-03-21 15:03:27 -0500

    chore: add emojis to logging

--- src/scripts/build.ts
+++ src/scripts/build.ts
@@ -6,16 +6,16 @@ import { $ } from "bun";
 const preflight = async () => {
   await $`rm -rf build`;
 
-  console.log("Generating Panda artifacts...");
+  console.log("🐼 Generating Panda artifacts...");
   await $`bun panda codegen`;
-  console.log("Panda artifacts generated.");
+  console.log("🐼 Panda artifacts generated.\n");
 };
 
 /**
  * Bundle the package.
  */
 const bundle = async () => {
-  console.log("Bundling...");
+  console.log("📦 Bundling...");
   await Bun.build({
     entrypoints: ["src/index.ts"],
     outdir: "build",
@@ -29,21 +29,21 @@ const bundle = async () => {
       "react-icons",
     ],
   });
-  console.log("Bundling complete.");
+  console.log("📦 Bundling complete.\n");
 };
 
 /**
  * Operations after bundling.
  */
 const postflight = async () => {
-  console.log("Generating type declarations...");
+  console.log("📘 Generating type declarations...");
   // TODO remove `tsup` dependency, should be able to use a Bun plugin or `tsc` directly (https://linear.app/omnidev/issue/OMNI-242/use-isolated-declarations-for-type-output-and-remove-tsup)
   await $`bun tsup src/index.ts --dts-only --tsconfig tsconfig.build.json --format esm --out-dir build`;
-  console.log("Type declarations generated.");
+  console.log("📘 Type declarations generated.\n");
 
-  console.log("Publishing local package...");
+  console.log("🧶 Publishing local package...");
   await $`bun knit push`;
-  console.log("Publishing local package complete.");
+  console.log("🧶 Publishing local package complete.");
 };
 
 /**
